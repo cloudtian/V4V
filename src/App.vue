3f43d7d8a3fcbecb1b3ea13b67df43433b5b1587
cloudtian <cloud_tian@foxmail.com>
2018-12-10 21:44:15 +0800

    [code]主页内容修改，使用导航菜单搭建基本框架

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,81 @@
 <template>
   <div id="app">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-    <router-view/>
+    <el-container>
+      <el-header id="header">v4v</el-header>
+    </el-container>
+    <el-container id="body">
+      <el-aside width="250px">
+        <el-menu :router="true" default-active="1" :style="{height: '100%'}">
+          <el-menu-item index="1" route="/">
+            <i class="el-icon-location"></i>
+            home
+          </el-menu-item>
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <lang>vue</lang>
+            </template>
+            <el-submenu index="2-1">
+              <template slot="title">
+                <lang>生命周期</lang>
+              </template>
+              <el-menu-item index="2-1-0" route="/lifecycle/overview">
+                <lang>概览</lang>
+              </el-menu-item>
+              <el-menu-item index="2-1-1">
+                <lang>created</lang>
+              </el-menu-item>
+              <el-menu-item index="2-1-2">
+                <lang>mounted</lang>
+              </el-menu-item>
+              <el-menu-item index="2-1-3">
+                <lang>updated</lang>
+              </el-menu-item>
+            </el-submenu>
+          </el-submenu>
+          <el-submenu index="3">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <lang>es6</lang>
+            </template>
+            <el-menu-item index="2-1" route="/about">
+              <template slot="title">
+                <lang>导入导出</lang>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+
+      <el-container>
+        <el-main>
+          <router-view/>
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
-}
+  name: "app"
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#header {
+  line-height: 60px;
+  height: 60px;
+  background: rgb(84, 92, 100);
+  color: white;
+}
+
+#body {
+    height: 500px;
+    border:1px solid #eee
 }
 </style>
