78cf6e87fb3cd7f258515c62fe56d6e5e700ffce
cloudtian <cloud_tian@foxmail.com>
2018-12-10 17:45:09 +0800

    [code]路由显示修改

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,8 @@
 <template>
   <div id="app">
-    <router-link to="/">Home</router-link> | 
+    <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
-    </div>
+    <router-view/>
   </div>
 </template>
 
