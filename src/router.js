760a18fe8fc111add9d2cc3a0e79d31362b8a3d3
cloudtian <cloud_tian@foxmail.com>
2018-12-15 18:19:33 +0800

    [comment]添加路由注释

--- src/router.js
+++ src/router.js
@@ -4,6 +4,11 @@ import Home from './views/Home.vue';
 
 Vue.use(Router);
 
+// 通过注入路由器，我们可以在任何组件内通过
+// this.$router 访问路由器，也可以通过 this.$route 访问当前路由：
+
+// 当 <router-link> 对应的路由匹配成功，将自动设置 class 属性值 .router-link-active
+
 export default new Router({
     mode: 'history',
     base: process.env.BASE_URL,
@@ -29,6 +34,12 @@ export default new Router({
                     component: () => import(/* webpackChunkName: "lifecycle" */ './views/lifecycle/index.vue')
                 }
             ]
+        },
+        {
+            // 动态路径参数以冒号开头
+            path: '/user/:id'
+
+            // 一个“路径参数”使用冒号 : 标记。当匹配到一个路由时，参数值会被设置到 this.$route.params
         }
     ]
 });
