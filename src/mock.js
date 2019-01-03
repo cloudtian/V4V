3050c149a848407046b577094e658dd0c0d5d25a
cloudtian <cloud_tian@foxmail.com>
2019-01-03 11:45:42 +0800

    [comment]mock comment format

--- src/mock.js
+++ src/mock.js
@@ -51,28 +51,28 @@ Mock.mock('/api/dynamic_data', {
  *
  */
 
- /**
-  * 生成规则和示例：
-  * 1. 属性值是字符串 String
-  * 'name|min-max': string 通过重复 string 生成一个字符串，重复次数大于等于 min，小于等于 max。
-  * 'name|count': string 通过重复 string 生成一个字符串，重复次数等于 count。
-  * 2. 属性值是数字 Number
-  * 'name|+1': number 属性值自动加 1，初始值为 number。
-  * 'name|min-max': number 生成一个大于等于 min、小于等于 max 的整数，属性值 number 只是用来确定类型。
-  * 'name|min-max.dmin-dmax': number 生成一个浮点数，整数部分大于等于 min、小于等于 max，小数部分保留 dmin 到 dmax 位。
-  * 3. 属性值是布尔型 Boolean
-  * 'name|1': boolean 随机生成一个布尔值，值为 true 的概率是 1/2，值为 false 的概率同样是 1/2
-  * 'name|min-max': value 随机生成一个布尔值，值为 value 的概率是 min / (min + max)，值为 !value 的概率是 max / (min + max)。
-  * 4. 属性值是对象 Object
-  * 'name|count': object 从属性值 object 中随机选取 count 个属性。
-  * 'name|min-max': object 从属性值 object 中随机选取 min 到 max 个属性。
-  * 5. 属性值是数组 Array
-  * 'name|1': array 从属性值 array 中随机选取 1 个元素，作为最终值。
-  * 'name|+1': array 从属性值 array 中顺序选取 1 个元素，作为最终值。
-  * 'name|min-max': array 通过重复属性值 array 生成一个新数组，重复次数大于等于 min，小于等于 max。
-  * 'name|count': array 通过重复属性值 array 生成一个新数组，重复次数为 count。
-  * 6. 属性值是函数 Function
-  * 'name': function 执行函数 function，取其返回值作为最终的属性值，函数的上下文为属性 'name' 所在的对象。
-  * 7. 属性值是正则表达式 RegExp
-  * 'name': regexp 根据正则表达式 regexp 反向生成可以匹配它的字符串。用于生成自定义格式的字符串。
-  */
+/**
+ * 生成规则和示例：
+ * 1. 属性值是字符串 String
+ * 'name|min-max': string 通过重复 string 生成一个字符串，重复次数大于等于 min，小于等于 max。
+ * 'name|count': string 通过重复 string 生成一个字符串，重复次数等于 count。
+ * 2. 属性值是数字 Number
+ * 'name|+1': number 属性值自动加 1，初始值为 number。
+ * 'name|min-max': number 生成一个大于等于 min、小于等于 max 的整数，属性值 number 只是用来确定类型。
+ * 'name|min-max.dmin-dmax': number 生成一个浮点数，整数部分大于等于 min、小于等于 max，小数部分保留 dmin 到 dmax 位。
+ * 3. 属性值是布尔型 Boolean
+ * 'name|1': boolean 随机生成一个布尔值，值为 true 的概率是 1/2，值为 false 的概率同样是 1/2
+ * 'name|min-max': value 随机生成一个布尔值，值为 value 的概率是 min / (min + max)，值为 !value 的概率是 max / (min + max)。
+ * 4. 属性值是对象 Object
+ * 'name|count': object 从属性值 object 中随机选取 count 个属性。
+ * 'name|min-max': object 从属性值 object 中随机选取 min 到 max 个属性。
+ * 5. 属性值是数组 Array
+ * 'name|1': array 从属性值 array 中随机选取 1 个元素，作为最终值。
+ * 'name|+1': array 从属性值 array 中顺序选取 1 个元素，作为最终值。
+ * 'name|min-max': array 通过重复属性值 array 生成一个新数组，重复次数大于等于 min，小于等于 max。
+ * 'name|count': array 通过重复属性值 array 生成一个新数组，重复次数为 count。
+ * 6. 属性值是函数 Function
+ * 'name': function 执行函数 function，取其返回值作为最终的属性值，函数的上下文为属性 'name' 所在的对象。
+ * 7. 属性值是正则表达式 RegExp
+ * 'name': regexp 根据正则表达式 regexp 反向生成可以匹配它的字符串。用于生成自定义格式的字符串。
+ */
