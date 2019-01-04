d51879f0d0589ba17f5031b15d2731461da58e69
cloudtian <cloud_tian@foxmail.com>
2019-01-04 10:16:32 +0800

    [code]update Menu.vue to activate the menu item according to this.$route.path

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -1,6 +1,8 @@
 <template>
-  <el-menu :router="true" default-active="1" :style="{height: '100%'}">
-    <el-menu-item index="1" route="/">
+  <el-menu :router="true"
+           :default-active="defaultActive"
+           :style="{height: '100%'}">
+    <el-menu-item index="/">
       <i class="el-icon-location"></i>
       home
     </el-menu-item>
@@ -11,7 +13,7 @@
       </template>
       <el-submenu index="2-1">
         <template slot="title">生命周期</template>
-        <el-menu-item index="2-1-0" route="/lifecycle-overview">概览</el-menu-item>
+        <el-menu-item index="/lifecycle-overview">概览</el-menu-item>
         <el-menu-item index="2-1-1">created</el-menu-item>
         <el-menu-item index="2-1-2">mounted</el-menu-item>
         <el-menu-item index="2-1-3">updated</el-menu-item>
@@ -28,7 +30,24 @@
         <i class="el-icon-location"></i>
         es6
       </template>
-      <el-menu-item index="2-1" route="/about">导入导出</el-menu-item>
+      <el-menu-item index="/about">导入导出</el-menu-item>
     </el-submenu>
   </el-menu>
 </template>
+
+<script>
+
+const DEFAULT_ACTIVE = '/';
+
+export default {
+
+    name: 'Menu',
+
+    data () {
+        return {
+            defaultActive: this.$route.path || DEFAULT_ACTIVE
+        };
+    }
+};
+
+</script>
