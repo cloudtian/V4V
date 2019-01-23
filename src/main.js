8b9a1f729781da5fa7800376e77e58beeeb93912
cloudtian <cloud_tian@foxmail.com>
2019-01-23 15:21:45 +0800

    [code]add progress when router changed

--- src/main.js
+++ src/main.js
@@ -4,11 +4,27 @@ import router from './router';
 import './plugins/element';
 import './plugins/axios';
 import './mock';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
 
 // 国际化支持
 import '@/i18n/index';
 
 Vue.config.productionTip = false;
+
+NProgress.configure({
+    showSpinner: false
+});
+
+// 在路由上绑定进度条
+router.beforeEach((to, from, next) => {
+    NProgress.start();
+
+    next();
+});
+router.afterEach(() => {
+    NProgress.done();
+});
 
 new Vue({
     router,
